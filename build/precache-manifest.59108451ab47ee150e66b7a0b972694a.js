32130430d4632559bc4451ea13f822e8e656a0b0
Daniel-Stafford <stafford.daniel@gmail.com>
2019-08-23 21:47:42 +0300

    added frontend build

---
+++ build/precache-manifest.59108451ab47ee150e66b7a0b972694a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2064737ca10c1601e1c8a13edf4b047d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f49a4aa95900918e691e",
+    "url": "/static/css/main.71d034df.chunk.css"
+  },
+  {
+    "revision": "104747ad14cfb66f9d59",
+    "url": "/static/js/2.9e57efb2.chunk.js"
+  },
+  {
+    "revision": "f49a4aa95900918e691e",
+    "url": "/static/js/main.bce7df6d.chunk.js"
+  },
+  {
+    "revision": "47aa8d6120bba8724ea0",
+    "url": "/static/js/runtime~main.54bcb109.js"
+  }
+]);
